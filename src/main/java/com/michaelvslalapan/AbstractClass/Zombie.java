cc48d026cec716529836414e3c01d5deb740755d
atarumar <athharumar03@gmail.com>
2024-05-07 11:05:14 +0700

    Update Zombie.java

--- src/main/java/com/michaelvslalapan/AbstractClass/Zombie.java
+++ src/main/java/com/michaelvslalapan/AbstractClass/Zombie.java
@@ -2,9 +2,12 @@ package main.java.com.michaelvslalapan.AbstractClass;
 
 public abstract class Zombie extends GameEntity {
     protected boolean is_aquatic;
+    protected boolean is_slowed;
 
-    public Zombie(String name, Integer health, Integer attack_damage, Integer attack_speed) {
+    public Zombie(String name, Integer health, Integer attack_damage, Integer attack_speed, boolean b, boolean c) {
         super(name, health, attack_damage, attack_speed);
+        is_aquatic = b;
+        is_slowed = c;
     }
 
     public abstract boolean isAquatic();
