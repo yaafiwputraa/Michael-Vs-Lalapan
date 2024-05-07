d717a001198a3a53702ab908c658227ec88c9ac3
atarumar <athharumar03@gmail.com>
2024-05-07 14:19:57 +0700

    Create PoleVaultingZombie.java

---
+++ src/main/java/com/michaelvslalapan/ZombieClass/PoleVaultingZombie.java
@@ -0,0 +1,40 @@
+package main.java.com.michaelvslalapan.ZombieClass;
+
+import main.java.com.michaelvslalapan.AbstractClass.Zombie;
+import main.java.com.michaelvslalapan.Interface.VaultingInterface;
+
+public class PoleVaultingZombie extends Zombie implements VaultingInterface {
+    private boolean hasVaulted;
+
+    public PoleVaultingZombie() {
+        super("Pole Vaulting Zombie", 175, 100, 1, false, false);
+        this.hasVaulted = false;
+    }
+
+    @Override
+    public boolean isAquatic() {
+        return this.is_aquatic;
+    }
+
+    @Override
+    public boolean isSlowed() {
+        return this.is_slowed;
+    }
+
+    @Override
+    public Zombie createZombie() {
+        return new PoleVaultingZombie();
+    }
+
+    @Override
+    public void vault() {
+        // INI NUNGGU TILE AMA GAMEMAP YAA
+        // NANTI DISESUAIIN
+        this.hasVaulted = true;
+    }
+
+    @Override
+    public boolean getVaulted() {
+        return this.hasVaulted;
+    }
+}
