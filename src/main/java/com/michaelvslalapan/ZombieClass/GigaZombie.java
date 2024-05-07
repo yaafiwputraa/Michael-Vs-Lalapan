cc0628bf69932ddbf9ba2c3c839bf9abb9ca5672
atarumar <athharumar03@gmail.com>
2024-05-07 14:20:02 +0700

    Create GigaZombie.java

---
+++ src/main/java/com/michaelvslalapan/ZombieClass/GigaZombie.java
@@ -0,0 +1,24 @@
+package main.java.com.michaelvslalapan.ZombieClass;
+
+import main.java.com.michaelvslalapan.AbstractClass.Zombie;
+
+public class GigaZombie extends Zombie {
+    public GigaZombie() {
+        super("GigaZombie", 400, 500, 4, false, false);
+    }
+
+    @Override
+    public boolean isAquatic() {
+        return this.is_aquatic;
+    }
+
+    @Override
+    public boolean isSlowed() {
+        return this.is_slowed;
+    }
+
+    @Override
+    public Zombie createZombie() {
+        return new GigaZombie();
+    }
+}
