e285a2c39b6fecde245b71130f16ed114ce01ee6
atarumar <athharumar03@gmail.com>
2024-05-07 11:04:55 +0700

    Create NormalZombie.java

---
+++ src/main/java/com/michaelvslalapan/ZombieClass/NormalZombie.java
@@ -0,0 +1,24 @@
+package main.java.com.michaelvslalapan.ZombieClass;
+
+import main.java.com.michaelvslalapan.AbstractClass.Zombie;
+
+public class NormalZombie extends Zombie {
+    public NormalZombie() {
+        super("Normal Zombie", 125, 100, 1, false, false);
+    }
+
+    @Override
+    public boolean isAquatic() {
+        return this.is_aquatic;
+    }
+
+    @Override
+    public boolean isSlowed() {
+        return this.is_slowed;
+    }
+
+    @Override
+    public Zombie createZombie() {
+        return new NormalZombie();
+    }
+}
