23f97f2b07c4f2db6e704bda1c68aa538951af1b
atarumar <athharumar03@gmail.com>
2024-05-07 14:22:15 +0700

    Create JackInTheBoxZombie.java

---
+++ src/main/java/com/michaelvslalapan/ZombieClass/JackInTheBoxZombie.java
@@ -0,0 +1,39 @@
+package main.java.com.michaelvslalapan.ZombieClass;
+
+import main.java.com.michaelvslalapan.AbstractClass.Zombie;
+import main.java.com.michaelvslalapan.Interface.Explode;
+
+public class JackInTheBoxZombie extends Zombie implements Explode {
+    private boolean hasExploded;
+
+    public JackInTheBoxZombie() {
+        super("JackInTheBox", 125, 1000, 1, false, false);
+        this.hasExploded = false;
+    }
+
+    @Override
+    public boolean isAquatic() {
+        return this.is_aquatic;
+    }
+
+    @Override
+    public boolean isSlowed() {
+        return this.is_slowed;
+    }
+
+    @Override
+    public Zombie createZombie() {
+        return new JackInTheBoxZombie();
+    }
+
+    @Override
+    public boolean isExploded() {
+        return this.hasExploded;
+    }
+
+    public void explode() {
+        // Implement the logic for exploding here.
+        // For now, we'll just set hasExploded to true.
+        this.hasExploded = true;
+    }
+}
